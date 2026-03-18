5d070e225138f6613b67e607432f0bb433fe0ff3
Net0ss <netanel.ossi@fiverr.com>
2026-03-18 13:14:07 +0200

    header

--- app/main.py
+++ app/main.py
@@ -177,6 +177,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["X-User-Balance"],
 )
 
 # ---------------------------------------------------------------------------
